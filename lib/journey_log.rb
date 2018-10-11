50884f2c9dc06f58608af879763685e42156af38
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-10-11 15:18:40 +0100

    added begin, finish and log methods to journey log

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,12 +1,23 @@
 require 'journey'
 
 class JourneyLog
-  def initialize(journey_class)
-    @journey_class = journey_class
+  def initialize
+    @journey_history = []
   end
 
 attr_reader :journey_class
 
+def begin(station)
+  @new_journey = Journey.new
+  @new_journey.start(station)
+end
 
+def finish(station)
+  @new_journey ||= Journey.new
+  @new_journey.end(station)
+  log
+end
 
+def log
+  @journey_history << @new_journey.journey_info
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,9 +1,8 @@
 require 'journey_log'
 
 describe JourneyLog do
-  it 'accepts journey_class as a parameter' do
-    journey = Journey.new
-    journey_log = JourneyLog.new(journey)
-    expect(journey_log.journey_class).to eq journey
-  end
+
+  let (:journey) {double :journey}
+
+  
 end
